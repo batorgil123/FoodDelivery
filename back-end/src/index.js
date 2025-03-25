d2be41e8ac086ffd2c84226303ce19d6dcc9a804
IHAW <imalreadylost9@gmail.com>
2025-03-24 21:29:12 -0700

    f

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -39,8 +39,8 @@ app.get("/", (req, res) => {
   res.send("Server is running.");
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 export default app;
