8a896bd30fee7f2025ec87294c0fb6683938b742
IHAW <imalreadylost9@gmail.com>
2025-03-26 20:39:39 -0700

    f

--- back-end/src/routes/foodorder-router.js
+++ back-end/src/routes/foodorder-router.js
@@ -3,7 +3,7 @@ import {createfoodorder} from '../CRUD/foodordercrud/createfoodorder.js'
 import {getOrder} from '../CRUD/foodordercrud/getfoodorder.js'
 import deletefoodorder from '../CRUD/foodordercrud/deletefoodorder.js'
 import putfoodorder from '../CRUD/foodordercrud/putfoodorder.js'
-import createfoodorderitem from '../CRUD/foodordercrud/createfoodorderitem.js'
+import {createfoodorderitem} from '../CRUD/foodordercrud/createfoodorderitem.js'
 const orderRouter = express.Router();
 
 orderRouter.post('/', createfoodorder);
